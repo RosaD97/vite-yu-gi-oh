c015badfe6abc0caa687eb554e786619424f8d64
RosaD97 <r960595@gmail.com>
2023-03-28 13:56:34 +0200

    aggiunto logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,8 @@ export default {
 </script>
 
 <template>
-    <header class="container ms_header">
+    <header class="container d-flex align-middle">
+        <img src="../../public/logo.png" alt="logo">
         <div>Yu-Gi-Oh-Api</div>
     </header>
 </template>
@@ -14,5 +15,9 @@ export default {
 <style lang="scss" scoped>
 div {
     font-size: 40px;
+}
+img{
+    height: 30px;
+    align-self: center;
 }
 </style>
